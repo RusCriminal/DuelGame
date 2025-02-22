ca17419fb47dba4a7a468ded8e1514a0fe577050
RusCriminal <143919047+RusCriminal@users.noreply.github.com>
2025-02-22 15:31:24 +0300

    Update player.py

--- entities/player.py
+++ entities/player.py
@@ -6,14 +6,51 @@ from settings import PLAYER_SPEED, PLAYER_HEALTH, PLAYER_JUMP_HEIGHT, PLAYER_GRA
 class Player(FirstPersonController):
     def __init__(self):
         super().__init__()
+        self.model = 'cube'  # Модель игрока
+        self.color = color.blue  # Цвет игрока
+        self.scale = (1, 2, 1)  # Масштаб игрока
+        self.position = (0, 1, 0)  # Позиция игрока
+        self.collider = 'box'  # Коллайдер для взаимодействий
+
+        # Настройки камеры
+        self.camera_pivot = Entity(parent=self)  # Точка вращения камеры
+        self.camera = Entity(parent=self.camera_pivot, position=CAMERA_POSITION)  # Камера
+        camera.parent = self.camera_pivot  # Привязка камеры к точке вращения
+        camera.position = CAMERA_POSITION  # Позиция камеры (вид от третьего лица)
+        camera.rotation = (0, 0, 0)  # Поворот камеры
+
+        # Настройки управления
+        self.mouse_sensitivity = Vec2(*CAMERA_SENSITIVITY)  # Чувствительность мыши
         self.cursor.visible = False  # Скрыть курсор
-        self.mouse_sensitivity = Vec2(40, 40)  # Чувствительность мыши
         self.gravity = PLAYER_GRAVITY
         self.jump_height = PLAYER_JUMP_HEIGHT
         self.health = PLAYER_HEALTH
         self.weapon = Weapon(parent=self)
         self.is_attacking = False
         self.is_blocking = False
+
+    def update(self):
+        self.handle_mouse_input()  # Обработка ввода мыши
+        self.handle_keyboard_input()  # Обработка ввода с клавиатуры
+
+    def handle_mouse_input(self):
+        # Вращение камеры с помощью мыши
+        if mouse.locked:
+            self.camera_pivot.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[0]
+            self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[1]
+            self.camera_pivot.rotation_x = max(-90, min(90, self.camera_pivot.rotation_x))  # Ограничение угла наклона
+
+
+    def handle_keyboard_input(self):
+        # Движение игрока
+        if held_keys['w']:
+            self.position += self.forward * PLAYER_SPEED * time.dt
+        if held_keys['s']:
+            self.position += self.back * PLAYER_SPEED * time.dt
+        if held_keys['a']:
+            self.position += self.left * PLAYER_SPEED * time.dt
+        if held_keys['d']:
+            self.position += self.right * PLAYER_SPEED * time.dt
 
     def take_damage(self, damage):
         if not self.is_blocking:
