f6d6460b59b997ba5b81f97ec8205039c028aeac
RusCriminal <143919047+RusCriminal@users.noreply.github.com>
2025-02-22 14:58:53 +0300

    Create player.py

---
+++ entities/player.py
@@ -0,0 +1,27 @@
+from ursina import FirstPersonController, Vec2, color
+from entities.weapon import Weapon
+from settings import PLAYER_SPEED, PLAYER_HEALTH, PLAYER_JUMP_HEIGHT, PLAYER_GRAVITY
+
+class Player(FirstPersonController):
+    def __init__(self):
+        super().__init__()
+        self.cursor.visible = False  # Скрыть курсор
+        self.mouse_sensitivity = Vec2(40, 40)  # Чувствительность мыши
+        self.gravity = PLAYER_GRAVITY
+        self.jump_height = PLAYER_JUMP_HEIGHT
+        self.health = PLAYER_HEALTH
+        self.weapon = Weapon(parent=self)
+        self.is_attacking = False
+        self.is_blocking = False
+
+    def take_damage(self, damage):
+        if not self.is_blocking:
+            self.health -= damage
+            print(f"Игрок получил урон! Здоровье: {self.health}")
+            if self.health <= 0:
+                self.die()
+
+    def die(self):
+        print("Игрок побежден!")
+        from ursina import application
+        application.quit()
