2b12fabb07d1f6d036630a2910c40ecbe26e9add
RusCriminal <143919047+RusCriminal@users.noreply.github.com>
2025-02-22 13:49:46 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+# settings.py
+
+# Настройки игрока
+PLAYER_SPEED = 5
+PLAYER_HEALTH = 100
+
+# Настройки противника
+ENEMY_SPEED = 2
+ENEMY_HEALTH = 100
+
+# Настройки оружия
+ATTACK_RANGE = 2
+ATTACK_DAMAGE = 10
