e58bf805996c4051158c27636c53e1d07217ef6c
RusCriminal <143919047+RusCriminal@users.noreply.github.com>
2025-02-22 14:33:15 +0300

    Update weapon.py

--- weapon.py
+++ weapon.py
@@ -1,4 +1,4 @@
-from ursina import Entity, color
+from ursina import Entity, color, lerp
 
 class Weapon(Entity):
     def __init__(self, parent, color=color.blue):
@@ -11,9 +11,17 @@ class Weapon(Entity):
             rotation=(0, 0, 45)
         )
         self.visible = False
+        self.attack_angle = 45  # Угол атаки
 
     def attack(self):
         self.visible = True
+        self.animate_rotation((0, 0, -self.attack_angle), duration=0.1)  # Анимация атаки
+        invoke(self.reset, delay=0.2)
+
+    def block(self):
+        self.visible = True
+        self.animate_rotation((0, 0, 90), duration=0.1)  # Анимация блокировки
 
     def reset(self):
         self.visible = False
+        self.rotation = (0, 0, 45)  # Возврат в исходное положение
