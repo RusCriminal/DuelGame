7e7bffd76e3486f355e33eb4cad4c52584538448
RusCriminal <143919047+RusCriminal@users.noreply.github.com>
2025-02-22 14:59:14 +0300

    Create enemy.py

---
+++ entities/enemy.py
@@ -0,0 +1,47 @@
+import random
+from ursina import Entity, distance, color, time, Vec3
+from entities.weapon import Weapon
+from settings import ENEMY_SPEED, ENEMY_HEALTH, ENEMY_DODGE_CHANCE, ATTACK_RANGE, ATTACK_DAMAGE
+
+class Enemy(Entity):
+    def __init__(self, target):
+        super().__init__(
+            model='cube',
+            color=color.red,
+            scale=(1, 2, 1),
+            position=(10, 1, 10),
+            collider='box'
+        )
+        self.health = ENEMY_HEALTH
+        self.target = target
+        self.weapon = Weapon(parent=self, color=color.magenta)
+        self.is_attacking = False
+        self.dodge_chance = ENEMY_DODGE_CHANCE
+
+    def update(self):
+        self.move_towards_target()
+        self.attack_target()
+        self.check_health()
+
+    def move_towards_target(self):
+        self.look_at(self.target.position)
+        self.position += self.forward * time.dt * ENEMY_SPEED
+
+    def attack_target(self):
+        if distance(self, self.target) < ATTACK_RANGE and not self.is_attacking:
+            self.is_attacking = True
+            self.weapon.attack()
+            if random.random() > self.dodge_chance:  # Игрок может уклониться
+                self.target.take_damage(ATTACK_DAMAGE)
+            self.is_attacking = False
+
+    def take_damage(self, damage):
+        self.health -= damage
+        print(f"Враг получил урон! Здоровье врага: {self.health}")
+        if self.health <= 0:
+            self.die()
+
+    def die(self):
+        print("Враг побежден!")
+        from ursina import application
+        application.quit()
